eddb55660d85fcc5ed0f202f8bc6203246ab7ec9
Ilya <ilyakonovalov12@gmail.com>
2023-07-08 17:06:42 +0400

    created login

--- django_app/apps/auth_app/urls.py
+++ django_app/apps/auth_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.auth_app.views import register
+from apps.auth_app.views import register, login_view
 # Create your tests here.
 
 urlpatterns = [
+    path('login/', login_view, name='login'),
     path('register/', register, name='register'),
 ]
